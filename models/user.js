0ee3c4d4b72034d1950e2ce587b591874406d921
Alec Petridis <alec@rgbsec.xyz>
2020-04-23 14:10:31 -0400

    Updated models+register page to use salted SHA256 instead of MD5

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const User = new mongoose.Schema({
   name: String,
   hash: String,
+  salt: String,
 });
 
 module.exports = mongoose.model('User', User);
--- routes/api/register/user.js
+++ routes/api/register/user.js
@@ -4,15 +4,34 @@ const crypto = require('crypto');
 const User = require('../../../models/user');
 
 const router = express.Router();
-const hasher = crypto.createHash('md5');
+
+
+const genSalt = function generateSaltOfSpecifiedLength(length) {
+  return crypto.randomBytes(Math.ceil(length / 2))
+    .toString('hex')
+    .slice(0, length);
+};
+
+const sha512 = function createSaltedSHA256Sum(password, salt) {
+  const hash = crypto.createHmac('sha512', salt);
+  hash.update(password);
+  const value = hash.digest('hex');
+  return {
+    salt,
+    passwordHash: value,
+  };
+};
+
 
 router.post('/', (req, res) => {
   debug(`register/user: ${JSON.stringify(req.body)}`);
-  hasher.update(req.body.password);
-  const hash = hasher.digest('hex');
+  const salt = genSalt(16);
+  const hash = sha512(req.body.password, salt);
+
   const user = new User({
     name: req.body.name,
     hash,
+    salt,
   });
   user.save((e) => {
     if (e) {
