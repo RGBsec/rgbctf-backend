5bc3142b6bd073d3af76e73624036a1c0c41750f
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2020-06-22 20:10:27 -0500

    I'm just going to plain do this now

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ mongoose
     debug(`mongoDB connected to on port ${r.connection.port}`);
   })
   .catch((e) => {
-    debug('Error connecting to mongodb');
+    console.error(`Error connecting to mongodb: ${e.message}`);
+    process.exit(-1);
   });
 
 app.use(helmet());
