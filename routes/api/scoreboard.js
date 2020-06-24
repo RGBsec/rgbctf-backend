14fb8a7c5f7eab0cc56a3352ca3f9813e37dc2fe
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2020-06-23 22:30:50 -0500

    Integrate error handler

--- routes/api/scoreboard.js
+++ routes/api/scoreboard.js
@@ -2,15 +2,14 @@ const express = require('express');
 const debug = require('debug')('rgbctf-backend');
 const Team = require('../../models/team');
 const Challenge = require('../../models/challenge');
+const createError = require('http-errors');
 
 const router = express.Router();
 
-router.get('/:index', (req, res) => {
+router.get('/:index', (req, res, next) => {
   const index = +req.params.index;
   if (index === undefined || Number.isNaN(index) || index === Infinity) {
-    res.send({ success: false, err: 'bad index' });
-    res.end();
-    return;
+    next(createError(404, 'Bad Index')); return;
   }
   Team.find({}, 'name points', {
     skip: index,
@@ -21,19 +20,16 @@ router.get('/:index', (req, res) => {
   }, (e, teams) => {
     if (e) {
       debug(`scoreboard/team: err: ${e}`);
-      res.send({ success: false, err: 'internal error' });
-      res.end();
+      next(createError(500, 'Internal Error'));
     } else {
       Challenge.find({}, 'points', (challE, challs) => {
         if (challE) {
           debug(`scoreboard/chall: err: ${challE}`);
-          res.send({ success: false, err: 'internal error' });
-          res.end();
-          return;
+          next(createError(500, 'Internal Error')); return;
         }
         res.send({
           success: true,
-          total: teams.length,
+          totalTeams: teams.length,
           maxPoints: challs.map(({ points }) => points).reduce((prev, curr) => prev + curr, 0),
           teams: teams.map(({ name, points }) => ({ name, points })),
         });
