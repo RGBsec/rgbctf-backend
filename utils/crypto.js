dba52efad6a9a70e07b8c7f59f17e4c0db42c97f
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2020-07-01 04:54:07 -0500

    [WIP] Work on the backend

--- utils/crypto.js
+++ utils/crypto.js
@@ -1,9 +1,12 @@
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 
 const hashPassword = (password, cost) => bcrypt.hash(password, cost);
 const checkPassword = (password, encodedPassword) => bcrypt.compare(password, encodedPassword);
+const secureRandom = (length) => crypto.randomBytes(length).toString('base64').replace(/\//g, '_').replace(/\+/g, '-');
 
 module.exports = {
   hashPassword,
   checkPassword,
+  secureRandom,
 };
